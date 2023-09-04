0822cc1e75d2a6d700f5eca3bf60a7954863dc99
craignelstead <ctnelst92@gmail.com>
2023-09-03 23:00:50 -0600

    Clicking favorite icon toggles favorite status icon

--- script.js
+++ script.js
@@ -5,22 +5,40 @@ const favsNodes = document.getElementsByClassName('iconstar');
 const eyesNodes = document.getElementsByClassName('iconeye');
 const sharesNodes = document.getElementsByClassName('iconshare');
 
+//Convert nodelists to arrays
 let favsArr = Array.from(favsNodes);
 let eyesArr = Array.from(eyesNodes);
 let sharesArr = Array.from(sharesNodes);
 
-let notifStatus = true;
-
+//Calls function to add event listeners
 listen();
 
+//Set default status of notification button
+let notifStatus = true;
+
+//Add event listeners
 function listen() {
     notif.addEventListener('click', toggleNotify);
 
-    favsArr.forEach((fav) => fav.addEventListener('click', toggleFav));
+    //Event listeners for favs
+    favsArr.forEach((fav) => fav.addEventListener('mouseover', (e) => hoverFavStart(e)));
+    favsArr.forEach((fav) => fav.addEventListener('mouseleave', (e) => hoverFavEnd(e)));
+    favsArr.forEach((fav) => fav.addEventListener('click', (e) => clickFav(e)));
+
     eyesArr.forEach((eye) => eye.addEventListener('click', toggleWatch));
-    sharesArr.forEach((share) => share.addEventListener('click', toggleShare));
+
+    //Sets image on hover for share button
+    sharesArr.forEach((share) => share.addEventListener('mouseover', (e) => {
+        e.target.src = './images/icons/share-variant.svg';}
+    ));
+
+    //Sets image on end hover for share button
+    sharesArr.forEach((share) => share.addEventListener('mouseleave', (e) => {
+        e.target.src = './images/icons/share-variant-outline.svg';}
+    ));
 }
 
+//Toggles notification status
 function toggleNotify() {
     if (notifStatus === true) {
         notif.src = './images/icons/bell-off-outline.svg';
@@ -31,14 +49,59 @@ function toggleNotify() {
     notifStatus = !notifStatus;
 }
 
-function toggleFav() {
+//Determine image source when hovering over favorite icon 
+function hoverFavStart(favEvent) {
+    let source = favEvent.target.getAttribute('src');
 
+    switch (source) {
+        case './images/icons/star-plus-outline.svg':
+            favEvent.target.src = './images/icons/star-plus.svg';
+            break;
+        case './images/icons/star-plus.svg':
+            favEvent.target.src = './images/icons/star-plus-outline.svg';
+            break;
+        case './images/icons/star-check-outline.svg':
+            favEvent.target.src = './images/icons/star-remove.svg';
+            break;
+    }
+    return;
+}
+
+//Determine image source when mouse leaves favorite icon
+function hoverFavEnd(favEvent) {
+    let source = favEvent.target.getAttribute('src');
+
+    switch (source) {
+        case './images/icons/star-plus.svg':
+            favEvent.target.src = './images/icons/star-plus-outline.svg';
+            break;
+        case './images/icons/star-remove.svg':
+            favEvent.target.src = './images/icons/star-check-outline.svg';
+            break;
+    }
+    return;
+}
+
+//Determine image source when clicking favorite icon
+function clickFav(favEvent) {
+    let source = favEvent.target.getAttribute('src');
+
+    switch (source) {
+        case './images/icons/star-plus.svg':
+            favEvent.target.src = './images/icons/star-remove.svg';
+            break;
+        case './images/icons/star-remove.svg':
+            favEvent.target.src = './images/icons/star-plus.svg';
+    }
+    return;
 }
 
 function toggleWatch() {
 
 }
 
-function toggleShare() {
-
+function toggleShare(e) {
+    if (e.target.src === './images/icons/share-variant-outline.svg') {
+        e.target.src = './images/icons/share-variant.svg'
+    }
 }
